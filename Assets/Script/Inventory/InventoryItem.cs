940dc391e3283d8aae9b60586303f70f8528800b
ElMarche <marcelojpelleriti@gmail.com>
2025-06-15 17:31:06 -0300

    Inventory progress
    Selectable inventory slots numbers 1 through 5.
    Selected slot allows to attack pressing left mouse button
    Medkit still not usable.
    Drag and drop works fine inside inventory.

--- Assets/Script/Inventory/InventoryItem.cs
+++ Assets/Script/Inventory/InventoryItem.cs
@@ -3,13 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+
     [Header("UI")]
     public Image image;
+    public  TextMeshProUGUI countText;
 
+    [HideInInspector] public Item item;
+    [HideInInspector] public int count = 1;
     [HideInInspector] public Transform parentAfterDrag;
+
+    // for testing purposes
+    /*
+    private void Start()
+    {
+        InitialiseItem(item);
+    }
+    */
+    
+    public void InitialiseItem(Item newItem)
+    {
+        item = newItem;
+        image.sprite= newItem.image;
+        RefreshCount();
+    }
+
+    public void RefreshCount()
+    {
+        countText.text = count.ToString();
+        bool textActive = count > 1;
+        countText.gameObject.SetActive(textActive);
+    }
+
+    // Drag and drop
     public void OnBeginDrag(PointerEventData eventData)
     {
         parentAfterDrag = transform.parent;
