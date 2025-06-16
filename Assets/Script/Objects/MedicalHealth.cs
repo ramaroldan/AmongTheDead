6afa9f5ea7a543ba09a8df4ff1965cda4620e41d
ramaroldan <ramironroldan@gmail.com>
2025-06-16 18:27:27 -0300

    sonidoKnife

--- Assets/Script/Objects/MedicalHealth.cs
+++ Assets/Script/Objects/MedicalHealth.cs
@@ -7,6 +7,22 @@ public class MedicalHealth : MonoBehaviour
     //[SerializeField] float countHelath; //cantidad de salud que da el objeto
     [SerializeField] Item item;
 
+    AudioSource audioS;
+
+    private void Start()
+    {
+        audioS = GetComponent<AudioSource>();
+    }
+
+    /*private void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            audioS.Play();
+        }
+    }*/
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player" && other.GetComponent<PlayerHealth>())
