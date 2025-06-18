0d26de1b12269c8f6359e26e19bc38c093b33312
ElMarche <marcelojpelleriti@gmail.com>
2025-06-18 01:13:37 -0300

    Item pickup sound
    Added pickup sound to WorldPistol, WorldRifle, and MedKit

--- Assets/Script/Objects/WorldPistol.cs
+++ Assets/Script/Objects/WorldPistol.cs
@@ -7,13 +7,24 @@ public class WorldPistol : MonoBehaviour
     [Header("Item Pistol")]
     public Item item;
 
+    [Header("Components")]
+    AudioSource audioSource;
+    AudioClip audioClip;
+
+    void Awake()
+    {
+        audioSource= GetComponent<AudioSource>();
+        audioClip = audioSource.clip;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
             bool canAdd = InventoryManager.instance.AddItem(item);
-            if(canAdd )
+            if(canAdd)
             {
+                AudioSource.PlayClipAtPoint(audioClip, transform.position, 0.5f);
                 Destroy(gameObject);
             }
         }
